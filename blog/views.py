a7e10fee6b5bc05158120c953c00515caf53f8fe
mukundch28 <mukund.ch28@gmail.com>
2019-06-17 15:06:31 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+# Create your views here.
+
+# posts = [
+#   {
+#        'author': 'Mukund',
+#       'title': 'Blog Post',
+#        'content': 'First test post',
+#        'date_posted': 'June 14'
+#    }
+#]
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
